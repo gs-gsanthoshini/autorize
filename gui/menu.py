645547c630953e5122e6812d65183864e55e967d
gs-gsanthoshini <gsanthoshini@gainsight.com>
2025-10-13 15:48:20 +0530

    Add verb swap context menu with submenu for all verb mappings

--- gui/menu.py
+++ gui/menu.py
@@ -5,10 +5,13 @@ from burp import IContextMenuFactory
 
 from java.util import LinkedList
 from javax.swing import JMenuItem
+from javax.swing import JMenu
 from java.awt.event import ActionListener
 
 from authorization.authorization import send_request_to_autorize
 from helpers.http import get_cookie_header_from_message, get_authorization_header_from_message
+from helpers.verb_swap import swap_http_verb, get_verb_mappings, get_verb_from_request
+from helpers.http import IHttpRequestResponseImplementation
 
 from thread import start_new_thread
 
@@ -23,14 +26,26 @@ class MenuImpl(IContextMenuFactory):
             requestMenuItem = JMenuItem("Send request to Autorize")
             cookieMenuItem = JMenuItem("Send Cookie header to Autorize")
             authMenuItem = JMenuItem("Send Authorization header to Autorize")
-
+            
+            # Create Verb Swap submenu
+            verbSwapMenu = JMenu("Swap HTTP Verb")
+            verb_mappings = get_verb_mappings()
+            
             for response in responses:
                 requestMenuItem.addActionListener(HandleMenuItems(self._extender,response, "request"))
                 cookieMenuItem.addActionListener(HandleMenuItems(self._extender, response, "cookie"))
                 authMenuItem.addActionListener(HandleMenuItems(self._extender, response, "authorization"))
+                
+                # Add verb swap menu items
+                for old_verb, new_verb in verb_mappings:
+                    verbSwapItem = JMenuItem("%s → %s" % (old_verb, new_verb))
+                    verbSwapItem.addActionListener(HandleVerbSwap(self._extender, response, old_verb, new_verb))
+                    verbSwapMenu.add(verbSwapItem)
+            
             ret.add(requestMenuItem)
             ret.add(cookieMenuItem)
             ret.add(authMenuItem)
+            ret.add(verbSwapMenu)
             return ret
         return None
 
@@ -49,3 +64,38 @@ class HandleMenuItems(ActionListener):
         
         if self._menuName == "authorization":
             self._extender.replaceString.setText(get_authorization_header_from_message(self._extender, self._messageInfo))
+
+class HandleVerbSwap(ActionListener):
+    """
+    Handler for verb swap context menu items
+    """
+    def __init__(self, extender, messageInfo, old_verb, new_verb):
+        self._extender = extender
+        self._messageInfo = messageInfo
+        self._old_verb = old_verb
+        self._new_verb = new_verb
+
+    def actionPerformed(self, e):
+        # Get the request
+        request = self._messageInfo.getRequest()
+        currentVerb = get_verb_from_request(self._extender._helpers, request)
+        
+        # Check if current verb matches
+        if currentVerb == self._old_verb:
+            # Swap the verb
+            swappedRequest = swap_http_verb(
+                self._extender._helpers, 
+                request, 
+                self._new_verb
+            )
+            
+            # Create new request response
+            httpService = self._messageInfo.getHttpService()
+            newRequestResponse = IHttpRequestResponseImplementation(
+                httpService, 
+                swappedRequest, 
+                None
+            )
+            
+            # Send through Autorize for testing
+            start_new_thread(send_request_to_autorize, (self._extender, newRequestResponse,))
